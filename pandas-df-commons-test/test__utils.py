73cb1ffd2238db2bd2b4a0f83ff340e5a0c669ea
KIC <KIC@KIC.KIC>
2022-12-07 18:34:29 +0000

    add patch utils

--- pandas-df-commons-test/test__utils.py
+++ pandas-df-commons-test/test__utils.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
+import numpy as np
+import pandas as pd
+
 from pandas_df_commons._utils.multiprocessing import streaming_parallel
+from pandas_df_commons._utils.patching import _add_functions
 
 
 class Test_Utils(TestCase):
@@ -16,3 +20,24 @@ class Test_Utils(TestCase):
             results,
             [x * 2 for x in range(l)]
         )
+
+    def test_Patch(self):
+        p1 = _add_functions('pandas_df_commons._utils.patching', filter=lambda x: x[1:] if x.startswith("_add") else None)(None)
+        self.assertIsNotNone(getattr(p1, 'add_functions', None))
+
+        p2 = _add_functions('pandas_df_commons._utils.patching', filter=lambda x: "foo." + x[1:] if x.startswith("_add") else None)(None)
+        self.assertIsNotNone(getattr(p2.foo, 'add_functions', None))
+
+        df = pd.DataFrame({1: np.linspace(10, 11, 5)})
+        p3 = _add_functions('pandas_df_commons.extensions.functions', filter=lambda x: "foo.bar.baz." + x)(df)
+
+        self.assertIsNotNone(getattr(p3.foo.bar.baz, 'cumpct_change', None))
+        np.testing.assert_almost_equal(
+            p3.foo.bar.baz.cumpct_change().values.squeeze(),
+            np.array([0.0, 0.0249, 0.04999, 0.07499, 0.09999]),
+            1.e-04
+        )
+
+        p4 = _add_functions('pandas_df_commons._utils.patching', filter=lambda x: "foo." + x[1:] if x.startswith("_add") else x)(None)
+        self.assertIsNotNone(getattr(p4.foo, 'add_functions', None))
+        self.assertIsNotNone(getattr(p4, '_monkey_patch_dataframe', None))
---
+++ pandas-df-commons/pandas_df_commons/_utils/patching.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import importlib
+from functools import partial, wraps
+from typing import Callable
+
+from pandas.core.base import PandasObject
+
+
+def _monkey_patch_dataframe(extension_field_name=None, extension_default_value=None, extension_class=None):
+    if extension_field_name is None:
+        extension_field_name = extension_default_value
+
+    existing = getattr(PandasObject, extension_field_name, None)
+    if existing is not None:
+        if not isinstance(existing, property) or not isinstance(existing.fget(None), extension_class):
+            raise ValueError(f"field already exists as {type(existing)}")
+
+    setattr(PandasObject, extension_field_name, property(lambda self: extension_class(self)))
+
+
+def _add_functions(*modules, filter: Callable[[str], str|None]):
+    functions = {}
+    for m in modules:
+        for name, func in importlib.import_module(m).__dict__.items():
+            if not callable(func):
+                continue
+
+            n = filter(name) if filter is not None else name
+            if n is not None:
+                functions[n] = func
+
+    class _Path(object):
+
+        def __init__(self,):
+            pass
+
+    class _Patch(object):
+
+        def __init__(self, df):
+            self.df = df
+
+            for name, func in functions.items():
+                path = name.split(".")
+                props_dict = self.__dict__
+
+                for i in range(len(path) -1):
+                    if path[i] not in props_dict:
+                        props_dict[path[i]] = _Path()
+
+                    props_dict = props_dict[path[i]].__dict__
+
+                props_dict[path[-1]] = wraps(func)(partial(func, self.df))
+
+    return _Patch
+
