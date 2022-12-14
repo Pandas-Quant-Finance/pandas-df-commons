1da423f67ccfa65eb4a6f5247a5acd512ce49e41
KIC <KIC@KIC.KIC>
2022-12-14 14:28:05 +0000

    enable multiindex level intersection

--- pandas-df-commons-test/test_indexing/test_index_intersection.py
+++ pandas-df-commons-test/test_indexing/test_index_intersection.py
@@ -16,3 +16,9 @@ class TestIndexIntersect(TestCase):
 
         self.assertListEqual([3, 4], index.tolist())
 
+    def test_intersection_and_lavel(self):
+        df1 = pd.DataFrame({}, index=[1, 2, 3, 4])
+        df2 = pd.DataFrame({}, index=pd.MultiIndex.from_product([[1, 2], [1, 2]]))
+
+        index = intersection_of_index(df1, df2, level=0)
+        self.assertListEqual([1, 2], index.tolist())
--- pandas-df-commons/pandas_df_commons/indexing/intersection.py
+++ pandas-df-commons/pandas_df_commons/indexing/intersection.py
@@ -1,11 +1,18 @@
+from __future__ import annotations
 import pandas as pd
 
 
-def intersection_of_index(*dfs: pd.DataFrame):
-    intersect_index = dfs[0].index
+def intersection_of_index(*dfs: pd.DataFrame | pd.Index, level=None):
+    intersect_index = dfs[0] if level is not None and isinstance(dfs[0], pd.Index) else dfs[0].index
+    if isinstance(intersect_index, pd.MultiIndex):
+        intersect_index = pd.unique(intersect_index.get_level_values(level))
 
     for i in range(1, len(dfs)):
         if dfs[i] is not None:
-            intersect_index = intersect_index.intersection(dfs[i].index)
+            other_index = dfs[i] if level is not None and isinstance(dfs[i], pd.Index) else dfs[i].index
+            if isinstance(other_index, pd.MultiIndex):
+                other_index = pd.unique(other_index.get_level_values(level))
+
+            intersect_index = intersect_index.intersection(other_index)
 
     return intersect_index.sort_values()
